fb7ba7da9ff72da12696b04ed684f6ba2a0675f8
Mohammed <bgd4500@gmail.com>
2024-12-21 23:35:16 +0200

    Feature: add google maps support

--- lib/features/home/home.dart
+++ lib/features/home/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:project_delphi/gen/assets.gen.dart';
 import 'package:project_delphi/widgets/button.dart' as custom;
 import 'package:project_delphi/widgets/floating_searchbar.dart';
@@ -24,12 +25,20 @@ class _HomeState extends State<Home> {
   final FocusNode _searchFocusNode = FocusNode();
   int? selectedLocationCardIndex;
   final ScrollController _locationCardScrollController = ScrollController();
-
+  late GoogleMapController _mapController;
   List<GlobalKey> itemKeys = List.generate(mockList.length, (_) => GlobalKey());
   List<int> itemWidths = [];
   int selectedCardOffset = 0;
   bool selectedCardCrossed = false;
+  Set<Marker> _markersList = {};
+  final CameraPosition initialCameraPosition = const CameraPosition(
+      zoom: 14, target: LatLng(31.908988679315637, 35.20323287752521));
   // bool selectedCardCrossedEnd = false;
+
+  void _onMapCreated(GoogleMapController controller) {
+    _mapController = controller;
+  }
+
   void updateSelectedCardState(bool hasCrossed) {
     if (selectedCardCrossed != hasCrossed) {
       setState(() {
@@ -125,6 +134,28 @@ class _HomeState extends State<Home> {
     }
   }
 
+  void _addMarker(LatLng position) {
+    final marker = Marker(
+      markerId: MarkerId(position.toString()),
+      position: position,
+      infoWindow: InfoWindow(
+        title: "Custom Location",
+        snippet: "${position.latitude}, ${position.longitude}",
+      ),
+    );
+
+    setState(() {
+      _markersList.clear();
+      _markersList.add(marker);
+    });
+  }
+
+  void _clearMarkers() {
+    setState(() {
+      _markersList.clear();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     bool isLocationCardSelected = selectedLocationCardIndex != null;
@@ -136,148 +167,223 @@ class _HomeState extends State<Home> {
           leadingIcon: leadingIcon,
           child: child,
         ),
-        body: Container(
-          width: double.infinity,
-          height: double.infinity,
-          decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(Assets.mapLight.path),
-              fit: BoxFit.cover,
+        body: Stack(
+          children: [
+            GoogleMap(
+              initialCameraPosition: initialCameraPosition,
+              markers: _markersList,
+              mapType: MapType.normal,
+              onMapCreated: _onMapCreated,
+              zoomGesturesEnabled: true,
+              scrollGesturesEnabled: true,
             ),
-          ),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              AnimatedSwitcher(
-                duration: duration,
-                child: isLocationCardSelected
-                    ? Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 32),
-                        child: Row(
-                          children: [
-                            Expanded(
-                              child: custom.Button(
-                                buttonStyle: custom.ButtonStyle.secondary,
-                                text: "Done",
-                                onPressed: () {/*TODO: move to next screen*/},
+            Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                AnimatedSwitcher(
+                  duration: duration,
+                  child: isLocationCardSelected
+                      ? Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 32),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: custom.Button(
+                                  buttonStyle: custom.ButtonStyle.secondary,
+                                  text: "Done",
+                                  onPressed: () {
+                                    /*TODO: move to next screen*/
+                                  },
+                                ),
                               ),
-                            ),
-                            const SizedBox(width: 8),
-                            CustomIconButton(
-                              icon: Icons.search_rounded,
-                              onPressed: () {
-                                onSearchTap();
-                              },
-                            )
-                          ],
+                              const SizedBox(width: 8),
+                              CustomIconButton(
+                                icon: Icons.search_rounded,
+                                onPressed: () {
+                                  onSearchTap();
+                                },
+                              )
+                            ],
+                          ),
+                        )
+                      : Visibility(
+                          maintainAnimation: true,
+                          maintainState: true,
+                          visible: !isSeparated,
+                          child: AnimatedOpacity(
+                            duration: const Duration(milliseconds: 500),
+                            opacity: isSeparated ? 0 : 1,
+                            child: GestureDetector(
+                                onTap: () {
+                                  onSearchTap();
+                                },
+                                child: const FloatingSearchbar()),
+                          ),
                         ),
-                      )
-                    : Visibility(
-                        maintainAnimation: true,
-                        maintainState: true,
-                        visible: !isSeparated,
-                        child: AnimatedOpacity(
-                          duration: const Duration(milliseconds: 500),
-                          opacity: isSeparated ? 0 : 1,
-                          child: GestureDetector(
-                              onTap: () {
-                                onSearchTap();
-                              },
-                              child: const FloatingSearchbar()),
+                ),
+                const SizedBox(height: 16),
+                Container(
+                  decoration: BoxDecoration(
+                    boxShadow: [
+                      BoxShadow(
+                          offset: const Offset(0, 5),
+                          color: Theme.of(context)
+                              .colorScheme
+                              .inverseSurface
+                              .withOpacity(0.1),
+                          blurRadius: 16,
+                          spreadRadius: 8)
+                    ],
+                  ),
+                  child: Stack(
+                    children: [
+                      SingleChildScrollView(
+                        controller: _locationCardScrollController,
+                        scrollDirection: Axis.horizontal,
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 32),
+                          child: Row(
+                            children: [
+                              ...List.generate(mockList.length, (index) {
+                                if (selectedCardCrossed &&
+                                    selectedLocationCardIndex == index) {
+                                  return SizedBox(
+                                    width: itemWidths[index].toDouble(),
+                                  );
+                                }
+                                return LocationChip(
+                                  key: itemKeys[index],
+                                  text: mockList[index].name,
+                                  isFavorite: index.isEven,
+                                  isSelected:
+                                      index == selectedLocationCardIndex,
+                                  onTap: () {
+                                    setState(() {
+                                      selectedCardCrossed = false;
+                                      if (selectedLocationCardIndex == index) {
+                                        selectedLocationCardIndex = null;
+                                        _clearMarkers();
+                                      } else {
+                                        selectedLocationCardIndex = index;
+                                        calculateSelectedCardOffset(index);
+                                        _addMarker(mockList[index].coords);
+                                        _mapController.animateCamera(
+                                          CameraUpdate.newLatLng(
+                                            mockList[index].coords,
+                                          ),
+                                        );
+                                      }
+                                    });
+                                  },
+                                );
+                              }),
+                            ],
+                          ),
                         ),
                       ),
-              ),
-              const SizedBox(height: 16),
-              Container(
-                decoration: BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                        offset: const Offset(0, 5),
-                        color: Theme.of(context)
-                            .colorScheme
-                            .inverseSurface
-                            .withOpacity(0.1),
-                        blurRadius: 16,
-                        spreadRadius: 8)
-                  ],
-                ),
-                child: Stack(
-                  children: [
-                    SingleChildScrollView(
-                      controller: _locationCardScrollController,
-                      scrollDirection: Axis.horizontal,
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(horizontal: 32),
-                        child: Row(
-                          children: [
-                            ...List.generate(mockList.length, (index) {
-                              if (selectedCardCrossed &&
-                                  selectedLocationCardIndex == index) {
-                                return SizedBox(
-                                  width: itemWidths[index].toDouble(),
-                                );
-                              }
-                              return LocationChip(
-                                key: itemKeys[index],
-                                text: mockList[index],
-                                isFavorite: index.isEven,
-                                isSelected: index == selectedLocationCardIndex,
+                      isLocationCardSelected && selectedCardCrossed
+                          ? Positioned(
+                              left: 32,
+                              child: LocationChip(
+                                key: const ValueKey("selected location card"),
+                                text: mockList[selectedLocationCardIndex!].name,
+                                isFavorite: selectedLocationCardIndex!.isEven,
+                                isSelected: true,
                                 onTap: () {
                                   setState(() {
-                                    selectedCardCrossed = false;
-                                    if (selectedLocationCardIndex == index) {
-                                      selectedLocationCardIndex = null;
-                                    } else {
-                                      selectedLocationCardIndex = index;
-                                      calculateSelectedCardOffset(index);
-                                    }
+                                    selectedLocationCardIndex =
+                                        null; // Unpin selected chip
+                                    _clearMarkers();
                                   });
                                 },
-                              );
-                            }),
-                          ],
-                        ),
-                      ),
-                    ),
-                    isLocationCardSelected && selectedCardCrossed
-                        ? Positioned(
-                            left: 32,
-                            child: LocationChip(
-                              key: const ValueKey("selected location card"),
-                              text: mockList[selectedLocationCardIndex!],
-                              isFavorite: selectedLocationCardIndex!.isEven,
-                              isSelected: true,
-                              onTap: () {
-                                setState(() {
-                                  selectedLocationCardIndex =
-                                      null; // Unpin logic
-                                });
-                              },
+                              ),
+                            )
+                          : const SizedBox(
+                              key: ValueKey("empty location chip"),
                             ),
-                          )
-                        : const SizedBox(
-                            key: ValueKey("empty location chip"),
-                          ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              const SizedBox(height: 32),
-            ],
-          ),
+                const SizedBox(height: 32),
+              ],
+            ),
+          ],
         ),
       ),
     );
   }
 }
 
-List<String> mockList = [
-  "Home",
-  "Work",
-  "Gym",
-  "School",
-  "Park",
-  "Cafe",
-  "Restaurant",
-  "Bar",
-  "Library",
+List<Location> mockList = [
+  const Location(
+      name: "Home",
+      coords: const LatLng(
+        31.905632,
+        35.204874,
+      ),
+      isFavorite: true),
+  const Location(
+    name: "Work",
+    coords: LatLng(
+      31.910254,
+      35.202134,
+    ),
+  ),
+  const Location(
+      name: "Gym",
+      coords: LatLng(
+        31.912774,
+        35.207893,
+      ),
+      isFavorite: true),
+  const Location(
+    name: "School",
+    coords: LatLng(
+      31.901374,
+      35.209456,
+    ),
+  ),
+  const Location(
+    name: "Park",
+    coords: LatLng(
+      31.903981,
+      35.206742,
+    ),
+  ),
+  const Location(
+    name: "Cafe",
+    coords: LatLng(
+      31.907124,
+      35.205118,
+    ),
+  ),
+  const Location(
+    name: "Restaurant",
+    coords: LatLng(
+      31.908712,
+      35.208956,
+    ),
+  ),
+  const Location(
+    name: "Bar",
+    coords: LatLng(
+      31.906543,
+      35.201732,
+    ),
+  ),
+  const Location(
+      name: "Library",
+      coords: LatLng(
+        31.909371,
+        35.203598,
+      ),
+      isFavorite: true),
 ];
+
+class Location {
+  final String name;
+  final LatLng coords;
+  final bool isFavorite;
+  const Location(
+      {required this.name, required this.coords, this.isFavorite = false});
+}
